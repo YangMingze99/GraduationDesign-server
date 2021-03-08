7b7dfdb4610bc0a4a245bda371184fd1b0d24a81
YangMingze99 <1735873875@qq.com>
2021-03-09 00:00:17 +0800

    master

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const jwt = require('jsonwebtoken')  //jwt
 const cors = require('cors')
 const authorization = require("./middleware/authorization.js")
-
+const bodyParser = require("body-parser");
 
 //后台路由
 const homeRouter = require('./routes/home.js');
@@ -17,29 +17,37 @@ const navItemRouter = require('./routes/foreground/navItems.js');
 const newsItemRouter = require('./routes/foreground/newsItems.js')
 
 const app = express();
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, 'public')));
 
 //跨域处理要在路由之前
-const whiteList = ["http://localhost:8080","http://192.168.31.98:8080","http://192.168.3.3:8080"];
+const whiteList = ["http://localhost:8080","http://192.168.31.129:8080","http://192.168.0.106:8080"];
 const corsOptions = {
 	origin:function(origin , callback){
-		// console.log(origin,'o');
+		console.log(origin,'o');
 		if(whiteList.indexOf(origin)!= -1){
 			callback(null,true)
 		}else{
-			callback(new Error('禁止访问'))
+			callback(new Error('禁止访问,跨域问题'))
 		}
 	}
 }
+
 app.use(cors(corsOptions));
+
 //前台url
 app.use('/navItem', navItemRouter);
 app.use('/newsItem', newsItemRouter);
 
+
+
+
+//权限验证
 app.use(authorization());
 
 //后台url
--- routes/foreground/newsItems.js
+++ routes/foreground/newsItems.js
@@ -1,7 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const navBarModel = require('../../model/foreground/newsItemModel.js');
+const newsItemModel = require('../../model/foreground/newsItemModel.js');
 
+router.get('/getNewsItem',function(req,res,next){
+	// console.log(req.query)
+	newsItemModel.find({
+		childrenId:req.query.childId
+	},{_id:1,newsTitle:1,newsText:1,newsPictures:1,update_time:1},(error,result)=>{
+		res.json({
+			code:200,
+			data:result
+		})
+	}).sort({ update_time:-1 })
+})
 
+router.get('/getNewsItemByParentId',function(req,res,next){
+	// console.log(req.query)
+	newsItemModel.find({
+		parentId:req.query.parentId
+	},{_id:1,newsTitle:1,newsText:1,newsPictures:1,update_time:1},(error,result)=>{
+		res.json({
+			code:200,
+			data:result
+		})
+	}).sort({ update_time:-1 })
+})
 
+router.get('/getSwiperBannerNews',function(req,res,next){
+	newsItemModel.find({
+		isBanner:true
+	},{_id:1,newsTitle:1,newsPictures:1,parentId:1,childrenId:1},(error,result)=>{
+		res.json({
+			code:200,
+			data:result
+		})
+	}).sort({ update_time:-1 })
+})
+
+router.get('/getNewsDetail',function(req,res,next){
+	newsItemModel.find({
+		_id:req.query.newsId
+	},(error,result)=>{
+		res.json({
+			code:200,
+			data:result
+		})
+	})
+})
 module.exports = router;
